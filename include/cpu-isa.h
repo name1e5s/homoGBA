6a4aa45b843c933173675a9e375491f6a9329af7
name1e5s <name1e5s@qq.com>
2019-02-12 20:45:29 +0800

    cpu: data processing(ALU) instructions with three operands

--- include/cpu-isa.h
+++ include/cpu-isa.h
@@ -182,4 +182,132 @@ static inline uint32_t ror_by_reg_carry(uint32_t value, uint8_t shift) {
   } else
     return value;
 }
+
+// ALU Instructions.
+#define ALU_3OP_IMM(INSN, N, Z, C, V, CARRY, PRE_BODY, BODY) \
+  static inline void arm_##INSN##_imm(uint32_t opcode) {     \
+    uint32_t Rd = (opcode >> 12) & 0xF;                      \
+    uint32_t Rn = (opcode >> 16) & 0xF;                      \
+    uint32_t val = opcode & 0xFF;                            \
+    uint32_t ror = (opcode >> 7) & 0x1E;                     \
+    PRE_BODY                                                 \
+    uint32_t Rn_val = cpu.R[Rn];                             \
+    if (Rn == R_PC)                                          \
+      Rn_val += 8;                                           \
+    uint32_t imm = build_imm(val, ror);                      \
+    BODY if (Rd == R_PC) cpu.R[Rd] -= 4;                     \
+  }                                                          \
+  static inline void arm_##INSN##s_imm(uint32_t opcode) {    \
+    uint32_t Rd = (opcode >> 12) & 0xF;                      \
+    uint32_t Rn = (opcode >> 16) & 0xF;                      \
+    uint32_t val = opcode & 0xFF;                            \
+    uint32_t ror = (opcode >> 7) & 0x1E;                     \
+    PRE_BODY                                                 \
+    uint32_t Rn_val = cpu.R[Rn];                             \
+    if (Rn == R_PC)                                          \
+      Rn_val += 8;                                           \
+    uint32_t imm = build_imm##CARRY(val, ror);               \
+    BODY F_N = (N);                                          \
+    F_Z = (Z);                                               \
+    F_C = (C);                                               \
+    F_V = (V);                                               \
+    if (Rd == R_PC) {                                        \
+      if (cpu.CPSR.M != MODE_USER) {                         \
+        cpu.CPSR = cpu.SPSR;                                 \
+        cpu_set_mode(cpu.SPSR.M);                            \
+      }                                                      \
+      cpu.R[Rd] -= 4;                                        \
+    }                                                        \
+  }
+
+ALU_3OP_IMM(and,
+            BIT(cpu.R[Rd], 31),
+            cpu.R[Rd] == 0,
+            F_C,
+            F_V,
+            _carry,
+            ,
+            cpu.R[Rd] = Rn_val & imm;)
+ALU_3OP_IMM(eor,
+            BIT(cpu.R[Rd], 31),
+            cpu.R[Rd] == 0,
+            F_C,
+            F_V,
+            _carry,
+            ,
+            cpu.R[Rd] = Rn_val ^ imm;)
+ALU_3OP_IMM(sub,
+            BIT(cpu.R[Rd], 31),
+            cpu.R[Rd] == 0,
+            (tmp >> 32) != 0,
+            V_ADD(Rn_val, (uint32_t)(uint64_t)(~imm), cpu.R[Rd]) != 0,
+            ,
+            ,
+            uint64_t tmp = (uint64_t)Rn_val + (uint64_t)(~imm) + 1ULL;
+            cpu.R[Rd] = (uint32_t)tmp;)
+ALU_3OP_IMM(rsb,
+            BIT(cpu.R[Rd], 31),
+            cpu.R[Rd] == 0,
+            (tmp >> 32) != 0,
+            V_ADD(imm, (uint32_t)(uint64_t)(~Rn_val), cpu.R[Rd]) != 0,
+            ,
+            ,
+            uint64_t tmp = (uint64_t)(~Rn_val) + (uint64_t)(imm) + 1ULL;
+            cpu.R[Rd] = (uint32_t)tmp;)
+ALU_3OP_IMM(add,
+            BIT(cpu.R[Rd], 31),
+            cpu.R[Rd] == 0,
+            (tmp >> 32) != 0,
+            V_ADD(Rn_val, imm, cpu.R[Rd]) != 0,
+            ,
+            ,
+            uint64_t tmp = (uint64_t)(Rn_val) + (uint64_t)(imm);
+            cpu.R[Rd] = (uint32_t)tmp;)
+ALU_3OP_IMM(adc,
+            BIT(cpu.R[Rd], 31),
+            cpu.R[Rd] == 0,
+            (tmp >> 32) != 0,
+            V_ADD(Rn_val, imm, cpu.R[Rd]) != 0,
+            ,
+            uint64_t prev_c = F_C;
+            , uint64_t tmp = (uint64_t)(Rn_val) + (uint64_t)(imm) + prev_c;
+            cpu.R[Rd] = (uint32_t)tmp;)
+ALU_3OP_IMM(sbc,
+            BIT(cpu.R[Rd], 31),
+            cpu.R[Rd] == 0,
+            (tmp >> 32) != 0,
+            V_ADD(Rn_val, (uint32_t)(uint64_t)(~imm), (uint32_t)tmp) != 0,
+            ,
+            uint64_t prev_c = F_C;
+            ,
+            uint64_t tmp = (uint64_t)Rn_val + (uint64_t)(~imm) + 1ULL - prev_c;
+            cpu.R[Rd] = (uint32_t)tmp;)
+ALU_3OP_IMM(rsc,
+            BIT(cpu.R[Rd], 31),
+            cpu.R[Rd] == 0,
+            (tmp >> 32) != 0,
+            V_ADD(imm, (uint32_t)(uint64_t)(~Rn_val), (uint32_t)tmp) != 0,
+            ,
+            uint64_t prev_c = F_C;
+            ,
+            uint64_t tmp = (uint64_t)(~Rn_val) + (uint64_t)(imm) + 1ULL -
+                           prev_c;
+            cpu.R[Rd] = (uint32_t)tmp;)
+ALU_3OP_IMM(orr,
+            BIT(cpu.R[Rd], 31),
+            cpu.R[Rd] == 0,
+            F_C,
+            F_V,
+            _carry,
+            ,
+            cpu.R[Rd] = Rn_val | imm;)
+ALU_3OP_IMM(bic,
+            BIT(cpu.R[Rd], 31),
+            cpu.R[Rd] == 0,
+            F_C,
+            F_V,
+            _carry,
+            ,
+            cpu.R[Rd] = Rn_val & (~imm);)
+            
 #endif
